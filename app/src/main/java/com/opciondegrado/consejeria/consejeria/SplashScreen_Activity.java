d2b510b8dd550e22f23b2c2042a418bce2b4a54d
Nexus339 <brayancorrea78@gmail.com>
2018-02-23 00:00:59 -0500

    Splash finish

--- app/src/main/java/com/opciondegrado/consejeria/consejeria/SplashScreen_Activity.java
+++ app/src/main/java/com/opciondegrado/consejeria/consejeria/SplashScreen_Activity.java
@@ -1,13 +1,35 @@
 package com.opciondegrado.consejeria.consejeria;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen_Activity extends AppCompatActivity {
-
+    private ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen_);
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation transition = AnimationUtils.loadAnimation(this,R.anim.transition);
+        iv.startAnimation(transition);
+        final Intent i = new Intent(this,MainActivity.class);
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }
