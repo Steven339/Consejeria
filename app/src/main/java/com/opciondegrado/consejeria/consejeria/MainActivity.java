a9f2d6cc37e757c94d77770da1567990c54344b0
Nexus339 <brayancorrea78@gmail.com>
2018-02-24 16:45:47 -0500

    Logueo y conexion terminada

--- app/src/main/java/com/opciondegrado/consejeria/consejeria/MainActivity.java
+++ app/src/main/java/com/opciondegrado/consejeria/consejeria/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         final TextView mTextView = (TextView) findViewById(R.id.textView);
         mTextView.setOnClickListener(this);
     }
