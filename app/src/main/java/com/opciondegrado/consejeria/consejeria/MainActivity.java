b57ab1bdb2a227282529d9a7e3da4c84ac1bfd18
Nexus339 <brayancorrea78@gmail.com>
2018-02-24 16:22:44 -0500

    Splash finish

--- app/src/main/java/com/opciondegrado/consejeria/consejeria/MainActivity.java
+++ app/src/main/java/com/opciondegrado/consejeria/consejeria/MainActivity.java
@@ -1,13 +1,54 @@
 package com.opciondegrado.consejeria.consejeria;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView mTextView = (TextView) findViewById(R.id.textView);
+        mTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText correo = (EditText) findViewById(R.id.correo);
+        EditText pass = (EditText) findViewById(R.id.pass);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.0.19/uniminuto/sesion.php?correo="+correo.getText().toString()+"&pass="+pass.getText().toString();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(!response.equals("[]")){
+                            Toast.makeText(getApplicationContext(),response,Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(getApplicationContext(),"Error",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),"error",Toast.LENGTH_LONG).show();
+            }
+        });
+        queue.add(stringRequest);
     }
 }
