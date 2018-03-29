cc191a3782a8b12c12a097761e6f7a03b45a70c6
Nexus339 <brayancorrea78@gmail.com>
2018-03-29 11:09:35 -0500

    Tus datos modulo activo para front

--- app/src/main/java/com/opciondegrado/consejeria/consejeria/InterfazPrinc.java
+++ app/src/main/java/com/opciondegrado/consejeria/consejeria/InterfazPrinc.java
@@ -1,13 +1,26 @@
 package com.opciondegrado.consejeria.consejeria;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+
 
 public class InterfazPrinc extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interfaz_princ);
+        LinearLayout datos = findViewById(R.id.tus_datos);
+        datos.setOnClickListener(new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (getApplicationContext (),TusDatos.class);
+                intent.addFlags (Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity (intent);
+            }
+        });
+
     }
 }
