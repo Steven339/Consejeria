2d5b5bf95261dc45605510f8d4c070128eb85f43
Nexus339 <brayancorrea78@gmail.com>
2018-03-29 19:37:13 -0500

    Tus datos modulo activo para front

--- app/src/main/java/com/opciondegrado/consejeria/consejeria/InterfazPrinc.java
+++ app/src/main/java/com/opciondegrado/consejeria/consejeria/InterfazPrinc.java
@@ -2,17 +2,41 @@ package com.opciondegrado.consejeria.consejeria;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 
-public class InterfazPrinc extends AppCompatActivity {
+
+public class InterfazPrinc extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    private LinearLayout datos;
+    GoogleApiClient googleApiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interfaz_princ);
-        LinearLayout datos = findViewById(R.id.tus_datos);
+
+        datos = findViewById(R.id.tus_datos);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder (GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail ()
+                .build ();
+
+
+        googleApiClient = new GoogleApiClient.Builder (this)
+                .enableAutoManage (this,this)
+                .addApi (Auth.GOOGLE_SIGN_IN_API,gso)
+                .build ();
+
         datos.setOnClickListener(new View.OnClickListener () {
             @Override
             public void onClick(View v) {
@@ -21,6 +45,42 @@ public class InterfazPrinc extends AppCompatActivity {
                 startActivity (intent);
             }
         });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart ( );
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn (googleApiClient);
+        if(opr.isDone ()){
+            GoogleSignInResult result =  opr.get ();
+            handleSignInResult(result);
+        }else{
+            opr.setResultCallback (new ResultCallback<GoogleSignInResult> ( ) {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+                    handleSignInResult (googleSignInResult);
+                }
+            });
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if(result.isSuccess ()){
+            GoogleSignInAccount account = result.getSignInAccount ();
+        }else{
+            goLoginScreen();
+        }
+    }
+
+    private void goLoginScreen() {
+        Intent intent = new Intent (this,MainActivity.class);
+        intent.addFlags (Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity (intent);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
---
+++ app/src/main/java/com/opciondegrado/consejeria/consejeria/TusDatos.java
@@ -0,0 +1,13 @@
+package com.opciondegrado.consejeria.consejeria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TusDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_tus_datos);
+    }
+}
